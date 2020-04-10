04eab776585e6ad4c6e15bc715bad254987e2883
StuartDambi <stuartdambi@gmail.com>
2020-04-11 00:39:12 +0300

    added nodemon and making first commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,8 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable linebreak-style */
+const covid19ImpactEstimator = () => {
+  // eslint-disable-next-line no-console
+  console.log('Running app');
+};
+covid19ImpactEstimator();
 
-export default covid19ImpactEstimator;
+module.exports = covid19ImpactEstimator;
